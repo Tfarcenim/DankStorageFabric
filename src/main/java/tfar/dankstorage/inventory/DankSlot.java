9dd9c82938ebd527893cb78febee5ea97be5d6d7
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2020-11-21 13:22:45 -0500

    are these methods even needed?

--- src/main/java/tfar/dankstorage/inventory/DankSlot.java
+++ src/main/java/tfar/dankstorage/inventory/DankSlot.java
@@ -1,10 +1,6 @@
 package tfar.dankstorage.inventory;
 
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.Slot;
-import net.minecraft.world.item.ItemStack;
-
-import javax.annotation.Nonnull;
 
 public class DankSlot extends Slot {
 
@@ -12,13 +8,4 @@ public class DankSlot extends Slot {
         super(itemHandler, index, xPosition, yPosition);
     }
 
-    @Override
-    public boolean mayPickup(Player playerIn) {
-        return true;
-    }
-
-    @Override
-    public int getMaxStackSize(@Nonnull ItemStack stack) {
-        return this.container.getMaxStackSize();
-    }
 }
